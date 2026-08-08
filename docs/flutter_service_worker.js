bfae3f0c6c95958f41ced24c73ecc2db0a10a5bd
khushi08singh <08singhkhushi11@gmail.com>
2026-08-08 12:40:42 +0530

    Fix GitHub Pages deployment

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "81ae9a7e9a78cbb82f5b7b97aa3261ba
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d50c13dce95a2faac6ea48ee9d296d30",
+"flutter_bootstrap.js": "f80729fbb165bacc102c531eefc3a6c0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
